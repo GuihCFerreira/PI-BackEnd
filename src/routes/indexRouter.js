95eae33ca39072fd58e1e36439eeb08e6f6ef153
GuihCFerreira <guihhh231@gmail.com>
2024-02-25 16:54:16 -0300

    ajuste vercel

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -4,6 +4,7 @@ const temperaturaRouter = require('./temperaturaRoutes');
 const umidadeRouter = require('./umidadeRoutes');
 const pressaoRouter = require('./pressaoRoutes');
 
+router.use('/', (req, res)=> res.status(200).json({"mesage":"tela inicial"}));
 router.use('/temperatura', temperaturaRouter);
 router.use('/umidade', umidadeRouter);
 router.use('/pressao', pressaoRouter);
